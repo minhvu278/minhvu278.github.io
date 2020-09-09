bdcd384eaa3b2537c43791e92fdaf765f98f42a0
vu <dominhvu278@gmail.com>
2020-09-09 14:58:34 +0700

    add project

---
+++ projects/react-shopping/precache-manifest.bbb67564ce9571c8f8d73612b0a92b6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb73afc8166d26679785d3a712de402d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbe1be09af105be3a528",
+    "url": "/static/css/main.e041e402.chunk.css"
+  },
+  {
+    "revision": "832084820852d044f1ca",
+    "url": "/static/js/2.30461436.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.30461436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe1be09af105be3a528",
+    "url": "/static/js/main.ec08cff6.chunk.js"
+  },
+  {
+    "revision": "75b67e50613f7dd4cc1e",
+    "url": "/static/js/runtime-main.5a1d5d0f.js"
+  }
+]);
